06b5ee889903d8fc0f61a9420a90551e15ce23af
Sunny Thaper <sthaper@gmail.com>
2017-06-18 22:29:07 -0700

    Initial commit

---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+MQTT_IP=xxx.xxx.xxx.xxx
+MQTT_PORT=1883
+MQTT_USER=username
+MQTT_PASSWORD=password
+COMMAND_TOPIC=tv/living_room
+STATE_TOPIC=tv/living_room/state
+
+cec_on() {
+  echo "on 0" | cec-client -s > /dev/null
+}
+
+cec_off() {
+  echo "standby 0" | cec-client -s > /dev/null
+}
+
+cec_status() {
+  while /bin/true; do
+    STATUS=$(echo 'pow 0' | cec-client -s | grep 'power status:')
+    OUTPUT="Transitioning"
+
+    if [ $STATUS = "power status: on" ]; then
+      OUTPUT="On"
+    fi
+    if [ $STATS = "power status: standby" ]; then
+      OUTPUT="Off"
+    fi
+
+    mosquitto_pub -r -h "$MQTT_IP" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASSWORD" -t "$STATE_TOPIC" -m "$OUTPUT" || true
+
+    sleep 5
+  done
+}
+
+cec_status &
+
+while read -r message
+do
+
+  case $message in
+  On)
+    cec_on &
+    ;;
+  Off)
+    cec_off &
+    ;;
+  esac
+
+done < <(mosquitto_sub -h "$MQTT_IP" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASSWORD" -t "$COMMAND_TOPIC" -q 1)
